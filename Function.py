2f9a3fda114f8343af3c5e88898e03288ec4f3b0
USER <liam.croguennec@efrei.net>
2026-03-14 17:11:57 +0100

    pandas + quelques commentaires

--- Function.py
+++ Function.py
@@ -1,10 +1,13 @@
 def txt_to_matrice(num):
     with open (f"Graphe{num}.txt","r") as f:
-        n = int(f.readline())
-        m = int(f.readline())
+        # Récolte des premières informations sur la matrice
+        n = int(f.readline())   #Nombre de bases de 0 n (lignes)
+        m = int(f.readline())   #Nombre d'associations dans le graphe
 
-        matrice = [[float('inf') for j in range(m)] for i in range(n)]
+        #Création de la matrice avec "inf" comme valeur de base
+        matrice = [[float('inf') for j in range(n)] for i in range(n)]
 
+        #Mise en place des différentes valeurs de la matrice
         for lines in f:
             u,v,w = map(int, lines.split())
             matrice[u][v] = w
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import pandas as pd
 from Function import *
 
-matrice = txt_to_matrice(13)
-print(matrice)
+matrice = txt_to_matrice(1)
+df = pd.DataFrame(matrice)
+print("Matrice initial : \n",df)
 
 floyd_warshall(matrice)
-print(matrice)
-
+new_df = pd.DataFrame(matrice)
+print("\nMatrice amélioré : \n",new_df)
